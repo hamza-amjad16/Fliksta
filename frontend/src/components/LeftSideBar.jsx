edf351fac3e02f33f0498fd9de5ddb1969a40a65
hamza-amjad16 <freeroomcardw@gmail.com>
2025-08-07 15:54:54 +0500

    Left Side bar page

--- frontend/src/components/LeftSideBar.jsx
+++ frontend/src/components/LeftSideBar.jsx
@@ -9,15 +9,20 @@ import {
 } from "lucide-react";
 import React from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { toast } from "sonner";
+import axios from "axios";
+import { USER_API } from "@/lib/const";
+import { useNavigate } from "react-router-dom";
 const SidebarItems = [
   { icon: <Home />, text: "Home" },
   { icon: <Search />, text: "Search" },
   { icon: <TrendingUp />, text: "Explore" },
   { icon: <MessageCircle />, text: "Messages" },
+  { icon: <Heart />, text: "Notifications" },
   { icon: <PlusSquare />, text: "Create" },
   {
     icon: (
-      <Avatar>
+      <Avatar className="w-6 h-6">
         <AvatarImage src="" alt="profile picture" />
         <AvatarFallback>CN</AvatarFallback>
       </Avatar>
@@ -27,6 +32,21 @@ const SidebarItems = [
   { icon: <LogOut />, text: "Logout" },
 ];
 const LeftSideBar = () => {
+  const navigate = useNavigate()
+  const logoutHandler = async () => {
+    try {
+      const res = await axios.get(`${USER_API}/logout`, {withCredentials: true})
+      if(res.data.success){
+        navigate("/login")
+        toast.success(res.data.message)
+      }
+    } catch (error) {
+      toast.error(error.response.data.message)  
+    }
+  }
+  const sidebarHandler = async (Texttype) => {
+    if(Texttype === "Logout") logoutHandler()
+  }
   return (
     <div className="fixed top-0 z-10 left-0 px-4 border-r border-gray-300 w-[16%] h-screen">
       <div className="flex flex-col">
@@ -34,7 +54,11 @@ const LeftSideBar = () => {
         <div>
           {SidebarItems.map((item, index) => {
             return (
-              <div className="flex items-center gap-4 relative" key={index}>
+              <div
+              onClick={() => sidebarHandler(item.text)}
+                className="flex items-center gap-3 relative hover:bg-gray-100  cursor-pointer rounded-lg p-4"
+                key={index}
+              >
                 {item.icon}
                 <span>{item.text}</span>
               </div>
